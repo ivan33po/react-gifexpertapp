91f30b83c8ab98e673310fc69567cf2e475e04c8
Ivan <ivancabeza85@gmail.com>
2020-07-22 13:06:33 +0200

    docs agregado

---
+++ docs/precache-manifest.c9b91b0ff08e2dd4ac257143ec43c622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d590d1de11a15644f6efd711e69d43e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1df70c539fbb44701b6f",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df70c539fbb44701b6f",
+    "url": "/static/js/main.84ec5eb6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
